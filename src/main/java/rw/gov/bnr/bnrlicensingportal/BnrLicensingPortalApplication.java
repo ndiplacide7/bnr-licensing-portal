ebba164dcda008a3ada969bbf0f2efc561512c33
Eng. Placide <ndiplacide7@gmail.com>
2026-05-09 12:10:30 +0200

    Initial project setup

---
+++ src/main/java/rw/gov/bnr/bnrlicensingportal/BnrLicensingPortalApplication.java
@@ -0,0 +1,13 @@
+package rw.gov.bnr.bnrlicensingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnrLicensingPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BnrLicensingPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rw/gov/bnr/bnrlicensingportal/BnrLicensingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.gov.bnr.bnrlicensingportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnrLicensingPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
